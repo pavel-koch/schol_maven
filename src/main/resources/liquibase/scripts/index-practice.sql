5a55fce3e641a70403ed94cec05f456b66b1a6c3
pavel <pavelkoch7@yandex.ru>
2024-12-28 10:54:34 +0300

    lvl4_SQL_3 ready

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -9,4 +9,8 @@ CREATE TABLE studentMig (
 CREATE TABLE facultyMig (
     id   SERIAL,
     name TEXT,
-    color TEXT)
+    color TEXT);
+
+CREATE INDEX student_name_index ON student(name);
+
+CREATE INDEX faculty_name_color_index ON faculty(name, color)
